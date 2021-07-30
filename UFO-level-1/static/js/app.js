3fbb1477bd042897871cf7a6e9b8acf8ae637033
Mighel Jackson <migheljackson@MacBook-Pro-29.home>
2021-07-30 16:16:47 -0400

    Added values to the table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -9,13 +9,18 @@ console.log(tableData);
 //     console.log(ufoReport);
 // });
 
+//for each ufo report, create a new row
 tableData.forEach(function (ufoReport) {
     console.log(ufoReport);
     var row = tbody.append("tr");
 
+    //for each entry in each ufo report, create a new cell
     Object.entries(ufoReport).forEach(function([key, value]) {
         console.log(key,value);
         var cell = row.append("td");
+
+        //add each value in ufo report to a successive cell
+        cell.text(value);
     });
 });
 
